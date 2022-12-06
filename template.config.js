8680954900534d5f743b50c778cb6ad546e7de54
Premshankar Shah <premshankar.shah@mindinventory.com>
2022-12-06 16:48:49 +0530

    init commit

---
+++ template.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "MIBoilerplate",
+
+  // Directory with template
+  templateDir: "./react-native-mi-calender",
+
+  // Path to script, which will be executed after init
+  // postInitScript: "./script.js"
+};
